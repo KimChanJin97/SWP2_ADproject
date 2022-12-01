2e977dea3605db9efd244d56e70607ee50af4d47
jodaun <awhekdns@kookmin.ac.kr>
2022-12-01 17:19:58 +0900

    update : random.py

--- random.py
+++ random.py
@@ -1,23 +1,30 @@
 import os
 import random
+class select :
+    def __init__(self):
+        self.dir_path = "C:\SWP_AD" # 파일의 경로
+        self.count = 0
+        self.images = []
+        
 
-dir_path = "C:\SWP_AD" # 파일의 경로
+    def randFromFile(self,file_path):
+        for (root, directories, files) in os.walk(self.dir_path): # 폴더 속의 파일이 무엇이 있는지 return
+            for file in files:
+                if '.png' in file:
+                    self.count += 1
+                    file_path = os.path.join(root, file)
+                    return file_path
 
-count = 0
-images = []
-for (root, directories, files) in os.walk(dir_path): # 폴더 속의 파일이 무엇이 있는지 return
-    for file in files:
-        if '.png' in file:
-            count += 1
-            file_path = os.path.join(root, file)
-            print(file_path)
+        for _ in range(self.count):
+            self.images.append(file_path)
+            return self.images
+            
+        result = random.sample(self.images,2) # 2 개의 이미지 경로를 랜덤하게 뽑아옴
 
-for _ in range(count):
-    images.append(file_path)
-print(images)
+        
+        
 
-r = random.randrange(count)
-print(images[r])
+
 
 
 

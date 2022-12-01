742c9663ae0d89131ebf5ebda3c9f1a0a5782168
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2022-12-01 22:43:37 +0900

    feat: create randomize method

--- randomize.py
+++ randomize.py
@@ -11,37 +11,8 @@ class Randomize(Crawl):
         
         path = 'crawled_img/' + self.word + '_' + self.maxImageNum
         dir_list = os.listdir(path) # 파일들을 리스트로 
-        print(random.sample(dir_list, k=int(self.maxImageNum)))
+        rand_dir_list = random.sample(dir_list, k=int(self.maxImageNum))
+        return rand_dir_list
         
-r = Randomize("dog", "10")
-r.randomize_image()
-
-  
-# import os
-# import random
-
-# class select :
-#     def __init__(self):
-#         self.dir_path = "C:\SWP_AD" # 파일의 경로
-#         self.count = 0
-#         self.images = []
-        
-
-#     def randFromFile(self,file_path):
-#         for (root, directories, files) in os.walk(self.dir_path): # 폴더 속의 파일이 무엇이 있는지 return
-#             for file in files:
-#                 if '.png' in file:
-#                     self.count += 1
-#                     file_path = os.path.join(root, file)
-#                     return file_path
-
-#         for _ in range(self.count):
-#             self.images.append(file_path)
-#             return self.images
-            
-#         result = random.sample(self.images,2) # 2 개의 이미지 경로를 랜덤하게 뽑아옴
-
-        
-        
-
-
+# r = Randomize("dog", "5")
+# r.randomize_image()

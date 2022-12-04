371858a05abf7896855a9d0a88eba07f4a4ef685
longZam <longZam305@gmail.com>
2022-12-04 16:42:30 +0900

    누락된 main.py 적용

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 from collageGUI import InputWindow
-# from crawl import Crawl
-# from randmoize import randomize
 from PyQt5 import QtWidgets
+from crawl import Crawl
+from randomize import randomize
+from manipulate import Manipulation
 import sys
 import tempfile
 
-def generateCollage(words: list, count: int):
+
+def generateCollage(mainImgPath: str, words: list, count: int):
     # 임시 폴더 경로 객체, 문자열 아님
     # 다른 함수에 매개변수로 전달할 때 반드시 문자열인 tempDirPath.name으로 전달해줄 것
-    tempDirPath = tempfile.TemporaryDirectory()
-
-    try:        
-        count = int(count)
-    except:
-        pass
-    
-    tempDirPath.cleanup()
-
-
+    with tempfile.TemporaryDirectory() as tempDirPath:
+        crawl = Crawl(words)
+        Manipulation(mainImgPath, randomize(crawl.crawl_image(tempDirPath), count))
 
 
 app = QtWidgets.QApplication(sys.argv)
 
 mainWindow = InputWindow()
 mainWindow.show()
-mainWindow.startButton.clicked.connect(lambda: generateCollage())
-
-
-# generateCollage()
-
+mainWindow.startButton.clicked.connect(lambda: generateCollage(mainWindow.imageLineEdit.text(), mainWindow.keywordLineEdit.text(), int(mainWindow.maxImageNumLineEdit.text())))
 
 sys.exit(app.exec_())
